5541ec755134c077a14c98ad508f1ed7f2ec0e00
Đỗ Kiệt <kietdo172@gmail.com>
2022-10-26 17:24:46 -0400

    haha

--- valid.js
+++ valid.js
@@ -5,20 +5,17 @@ var pass_error = document.getElementById('pass_error');
 
 password.addEventListener('textInput', pass_Verify);
 
-function validated(){
-	if (password.value.length < 6) {
-		password.style.border = "1px solid red";
-		pass_error.style.display = "block";
-		password.focus();
-		return false;
-	}
-
-}
 function pass_Verify(){
 	if (password.value === real_password) {
 		password.style.border = "1px solid silver";
 		pass_error.style.display = "none";
 		return true;
-	}else return false;
+	}else 
+	{
+		password.style.border = "1px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
 }
 
